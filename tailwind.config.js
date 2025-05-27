d43c52f14b2ca66238cdcd8e34608f264792d36f
sis061 <sis061@naver.com>
2025-05-27 23:47:10 +0900

    feat: 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  fontFamily: {
+    sans: ["Pretendard", "sans-serif"],
+  },
   theme: {
     extend: {
       colors: {

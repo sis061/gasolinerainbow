8b5e5ff8d9c5503843171e0a5490a674ce9e93dc
sis061 <sis061@naver.com>
2025-05-29 18:44:55 +0900

    feat: 홈 화면 수정중, 백그라운드 수정중

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,7 +4,15 @@ import Header from "./Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="transition-all duration-200 ease-in-out">
+    <main className="transition-all duration-200 ease-in-out relative">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        autoPlay
+        muted
+        loop
+        playsInline
+        src="/src/assets/vid/bg01.mp4"
+      />
       <Header />
       {children}
       <Footer />
--- src/layout/sections/Home.tsx
+++ src/layout/sections/Home.tsx
@@ -1,5 +1,13 @@
 import YouTube from "react-youtube";
 
+import BandcampLogo from "@/assets/logos/bandcamp.svg?react";
+import ApplemusicLogo from "@/assets/logos/applemusic.svg?react";
+import SpotifyLogo from "@/assets/logos/spotify.svg?react";
+import YoutubeMusicLogo from "@/assets/logos/youtubemusic.svg?react";
+import BugsLogo from "@/assets/logos/Bugs_BI_wordmark_white.svg?react";
+import MelonLogo from "@/assets/logos/melonLogo.svg?react";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   // const today: number = +new Date();
   //TODO : 발매 날짜 정해지면 발매 전에는 타이머, 발매 후에는 앨범 소개 페이지로 전환 기능 추가
@@ -7,13 +15,13 @@ export default function Home() {
 
   return (
     <section className="wrapper w-full min-h-[calc(100dvh-8rem)] overflow-x-hidden !mx-auto flex justify-center">
-      <div className="inner flex-grow-0 w-full bg-white !my-10 !p-6 shadow-2xl">
+      <div className="inner flex-grow-0 w-full !my-10 !p-6 ">
         {!isReleased ? (
           <>
-            <div className="grid grid-rows-2 grid-cols-3 w-full h-full gap-2">
+            <div className="grid grid-rows-2 grid-cols-4 w-full h-full gap-2 ">
               {/* TODO: 앨범 소개 컴포넌트 구분해서 만들기 */}
-              <div className="row-start-2 col-start-1 col-span-3  flex flex-col gap-10 !-mt-18 !p-6 !pr-1">
-                <h1 className="text-2xl font-bold">
+              <div className="shadow-2xl row-start-2 col-start-1 col-span-4 bg-white flex flex-col justify-between gap-10 !-mt-18 !mr-12 !p-6 ">
+                <h1 className="text-lg font-bold !p-2">
                   앨범 <span className="text-4xl">[ HM ]</span>
                 </h1>
                 {/* TODO: 소개글 elpsis 적용하기 // 전체보기로 할 건지 디스코그라피로 이동시킬건지 고민 */}
@@ -26,18 +34,67 @@ export default function Home() {
                   가끔은 초연해지는, 저 그리고 모두가 가진 본연의 모습을 담고
                   싶었습니다.
                 </p>
-                <div>
-                  <h2 className="text-xl font-bold">아래에서 듣기</h2>
-                  <ul className="">
-                    <li>스포티파이</li>
-                    <li>멜론</li>
-                    <li>애플뮤직</li>
-                    <li>벅스</li>
-                    <li>유튜브뮤직</li>
+                <div className="flex flex-col gap-6">
+                  <h2 className="font-bold !pl-1">아래에서 듣기</h2>
+                  <ul className="flex gap-3">
+                    <li className="">
+                      <Button
+                        variant="default"
+                        className="!p-3 bg-[#0687F5] hover:bg-[#0687F5]/75 cursor-pointer"
+                      >
+                        <BandcampLogo fill="#fff" />
+                        <span className="!text-white">밴드캠프</span>
+                      </Button>
+                    </li>
+                    <li className="">
+                      <Button
+                        variant="default"
+                        className="!p-3 bg-[#81b71a] hover:bg-[#81b71a]/75 cursor-pointer"
+                      >
+                        <SpotifyLogo fill="#fff" />
+                        <span className="!text-white">스포티파이</span>
+                      </Button>
+                    </li>
+                    <li className="">
+                      <Button
+                        variant="default"
+                        className="!p-3 bg-[#CD201F] 75ver:bg-[#CD201F]/75 cursor-pointer"
+                      >
+                        <YoutubeMusicLogo fill="#fff" />
+                        <span className="!text-white">유튜브 뮤직</span>
+                      </Button>
+                    </li>
+                    <li className="">
+                      <Button
+                        variant="default"
+                        className="!p-3 bg-[#f94c57] hover:bg-[#f94c57]/75 cursor-pointer"
+                      >
+                        <ApplemusicLogo fill="#fff" />
+                        <span className="!text-white">애플 뮤직</span>
+                      </Button>
+                    </li>
+                    <li className="">
+                      <Button
+                        variant="default"
+                        className="!p-3 bg-[#03CD3D] hover:bg-[#03CD3D]/75 cursor-pointer"
+                      >
+                        <MelonLogo fill="#fff" />
+                        <span className="!text-white">멜론</span>
+                      </Button>
+                    </li>
+                    <li className="">
+                      <Button
+                        variant="default"
+                        className="!p-3 bg-[#FF3B28] hover:bg-[#FF3B28]/75 cursor-pointer"
+                      >
+                        <BugsLogo fill="#fff" />
+                        <span className="!text-white">벅스</span>
+                      </Button>
+                    </li>
                   </ul>
                 </div>
               </div>
-              <div className="row-start-1 overflow-hidden col-start-2 col-span-2 bg-green-300 w-full h-full flex items-center justify-center [&_>div]:w-full [&_>div]:h-full">
+              <div className="row-start-1 overflow-hidden col-start-2 col-span-3 bg-black w-full h-full flex items-center justify-center [&_>div]:w-full [&_>div]:h-full shadow-xl">
                 <HMYoutubeEmbed />
               </div>
             </div>

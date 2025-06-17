6ee53723022760fef562c008982f9998cc6a3be5
sis061 <sis061@naver.com>
2025-06-17 16:11:41 +0900

    feat: 토스터기능 및 커스텀 컴포넌트 추가 fix: 디스코그라피 > UI 변경, 가사 없음 아이콘 삭제.

---
+++ src/pages/Layout/components/CustomToast.tsx
@@ -0,0 +1,29 @@
+import type { ReactElement } from "react";
+
+import { toast } from "sonner";
+import { X } from "lucide-react";
+
+export interface CustomToastProps {
+  t: string | number;
+  icon: ReactElement;
+  content: string;
+}
+
+const CustomToast = ({ t, icon, content }: CustomToastProps) => {
+  return (
+    <div className="flex flex-col min-w-80 bg-accent !px-6 !py-4 relative border-1 border-black/75">
+      <button
+        onClick={() => toast.dismiss(t)}
+        className="absolute top-2 right-2 cursor-pointer hover:animate-pulse"
+      >
+        <X size={16} color="#000" />
+      </button>
+      <div className="flex items-center justify-start gap-1 w-full">
+        {icon}
+        <span className="!text-black text-sm">{content}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CustomToast;
